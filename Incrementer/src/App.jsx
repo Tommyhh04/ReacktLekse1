c780acbb915f7219259b828b6604adadde45458f
Eggcanoe <tommyhalleland@hotmail.com>
2023-10-11 09:47:03 +0200

    Added styling

--- Incrementer/src/App.jsx
+++ Incrementer/src/App.jsx
@@ -4,14 +4,20 @@ function App() {
  
 
   return (
-    <div>
-
-      {/*Main content*/}
+    <div className="grid--card">
+    <div className="incrementer--card">
+       {/*Main content*/}
+      <div className="incrementer--title">
         <h3>Incrementer or subtracter</h3>
-        <button>⬆️</button>
+       </div>
+      {/*Displays the buttons and number that will be changed on button press*/}
+        <div className="incrementer--buttons">
+        <button className="minus">⬇️</button>
         <h2>0</h2>
-        <button>⬇️</button>
+        <button className="plus">⬆️</button>
+        </div>
     </div>
+</div>
   )
 }
 
