aba0226f7b8200afaa6668f6704e04cdc9d06345
Eggcanoe <tommyhalleland@hotmail.com>
2023-10-11 09:06:06 +0200

    first commit

---
+++ Incrementer/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+
+      {/*Main content*/}
+        <h3>Incrementer or subtracter</h3>
+        <button>⬆️</button>
+        <h2>0</h2>
+        <button>⬇️</button>
+    </div>
+  )
+}
+
+export default App
