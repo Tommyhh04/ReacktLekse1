8d6f6ac01794092aa17ff0ce20c1dba00d77aaae
Eggcanoe <tommyhalleland@hotmail.com>
2023-10-11 10:18:33 +0200

    Added files to Components

--- Incrementer/src/App.jsx
+++ Incrementer/src/App.jsx
@@ -1,23 +1,13 @@
 import './App.css'
+import Incrementer from "./Components/Incrementer"
 
 function App() {
  
 
   return (
-    <div className="grid--card">
-    <div className="incrementer--card">
-       {/*Main content*/}
-      <div className="incrementer--title">
-        <h3>Incrementer or subtracter</h3>
-       </div>
-      {/*Displays the buttons and number that will be changed on button press*/}
-        <div className="incrementer--buttons">
-        <button className="minus">⬇️</button>
-        <h2>0</h2>
-        <button className="plus">⬆️</button>
-        </div>
-    </div>
-</div>
+   <div>
+    <Incrementer/>
+   </div>
   )
 }
 
---
+++ Incrementer/src/Components/Incrementer.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Incrementer(){
+<div className="grid--card">
+<div className="incrementer--card">
+   {/*Main content*/}
+  <div className="incrementer--title">
+    <h3>Incrementer or subtracter</h3>
+   </div>
+  {/*Displays the buttons and number that will be changed on button press*/}
+    <div className="incrementer--buttons">
+    <button className="minus">⬇️</button>
+    <h2>0</h2>
+    <button className="plus">⬆️</button>
+    </div>
+</div>
+</div>
+}
